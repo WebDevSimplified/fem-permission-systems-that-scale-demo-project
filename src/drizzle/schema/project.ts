181f1e70c70530d37c59b2866b2be7022cc6b29f
Web Dev Simplified <kyle@webdevsimplified.com>
2026-01-07 09:08:37 -0600

    DB Setup

---
+++ src/drizzle/schema/project.ts
@@ -0,0 +1,28 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core"
+import { createdAt, id, updatedAt } from "../schemaHelpers"
+import { relations } from "drizzle-orm"
+import { UserTable } from "./user"
+import { DocumentTable } from "./document"
+
+export const ProjectTable = pgTable("projects", {
+  id,
+  name: text().notNull(),
+  description: text(),
+  ownerId: uuid()
+    .notNull()
+    .references(() => UserTable.id, { onDelete: "cascade" }),
+  department: text(),
+  createdAt,
+  updatedAt,
+})
+
+export const ProjectRelationships = relations(
+  ProjectTable,
+  ({ many, one }) => ({
+    owner: one(UserTable, {
+      fields: [ProjectTable.ownerId],
+      references: [UserTable.id],
+    }),
+    documents: many(DocumentTable),
+  }),
+)
